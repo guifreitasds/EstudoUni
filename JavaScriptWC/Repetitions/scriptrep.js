e5e54ce45299522aeceb0068f238c42189320817
Guilherme Freitas <guilhermefsantos9@gmail.com>
2022-11-22 19:24:11 -0300

    Finalizando ex002 rep

--- JavaScriptWC/Repetitions/scriptrep.js
+++ JavaScriptWC/Repetitions/scriptrep.js
@@ -17,3 +17,20 @@ function fatorial(num) {
     }
     res.innerHTML = `O resultado do fatorial de ${numesc} é ${Number(total)}`;
 }
+
+
+function storebonus() {
+    var name = document.getElementById('nameid').value;
+    var total = document.getElementById('valuebuyid').value;
+    var res = document.getElementById('resbonus');
+
+    if(total>=10000){
+        var percentbonus = 15;
+        var bonus = total*(percentbonus/100);
+    } else{
+        percentbonus = 10;
+        bonus = total*(percentbonus/100);
+    }
+
+    res.innerHTML = `Olá, ${name}. Você recebeu um bônus de ${percentbonus}% em cima do valor total das suas compras no ano, com o total de R$${bonus}`
+}
