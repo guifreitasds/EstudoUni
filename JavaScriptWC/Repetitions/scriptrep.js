e7397de979ea0909945effb81fba414b275497be
Guilherme Freitas <guilhermefsantos9@gmail.com>
2022-11-22 18:57:13 -0300

    Finalizando ex 001 rep

--- JavaScriptWC/Repetitions/scriptrep.js
+++ JavaScriptWC/Repetitions/scriptrep.js
@@ -1,6 +1,19 @@
+// Lista de laços repetitivos
+
 function fatorial(num) {
-    while (num>1) {
-        num*(num-1);
-        num--;
+
+    // Exercicio 001
+
+    var res = document.getElementById('resfatorial');
+    var numesc = num;
+    var total = num;
+    // while (num>1) {
+    //     num--;
+    //     total *= num;
+    // }
+
+    for(i=num-1;i>1;i--){
+        total*=i;
     }
+    res.innerHTML = `O resultado do fatorial de ${numesc} é ${Number(total)}`;
 }
