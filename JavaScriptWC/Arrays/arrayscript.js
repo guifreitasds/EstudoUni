77dcb26040e894367443d813fc66eb0ddbb533dc
Guilherme Freitas <guilhermefsantos9@gmail.com>
2022-12-01 21:45:42 -0300

    Finalizando ex002 e 003

--- JavaScriptWC/Arrays/arrayscript.js
+++ JavaScriptWC/Arrays/arrayscript.js
@@ -1,14 +1,21 @@
 function createArray(tam) {
     var i=0;
+    var iaux = 0;
     var lista = [];
+    var listavogal = [];
     while (true){
         if(i==tam){
             break;
         }
         var valor = window.prompt('Digite o nome a ser inserido na lista')
+        console.log(valor[0].toLowerCase())
+        if(valor[0].toLowerCase() == 'a' || valor[0].toLowerCase() == 'e' || valor[0].toLowerCase() == 'i' || valor[0].toLowerCase() == 'o' || valor[0].toLowerCase() == 'u'){
+            listavogal[iaux] = valor;
+            iaux++; 
+        }
         lista[i] = valor;
         i++
-        console.log(tam)
     }
-    console.log(lista)
+    console.log(lista.sort())
+    console.log(listavogal.sort())
 }
