1d816c729439090fdfaaef483e646c1b296a8916
Guilherme Freitas <guilhermefsantos9@gmail.com>
2022-12-03 10:37:55 -0300

    Implementando 3° programa simples de console com prompts :(

--- JavaScriptWC/Arrays/arrayscript.js
+++ JavaScriptWC/Arrays/arrayscript.js
@@ -154,3 +154,67 @@ function Users() {
         
     } while(opc!=6);
 }
+
+function buyStoreAward() {
+    var matriz = [];
+    do {
+        var opc = Number(window.prompt(`
+            Escolha a opção desejada
+            [1]-Inserir novo registro
+            [2]-Ver registros
+            [3]-Procurar registros por nome
+            [4]-Ver prêmios do registro pelo nome
+            [5]-Sair
+        `));
+        switch (opc) {
+            case 1:
+                var name = window.prompt("Digite o nome do cliente");
+                var qtdpdt = window.prompt("Digite a quantidade de produtos comprados");
+                var total = Number(window.prompt("Digite o valor total da compra"));
+                var listaaux = []
+                listaaux.push(name, qtdpdt, total);
+                matriz.push(listaaux)
+                break;
+            case 2:
+                for(i=0;i<matriz.length;i++){
+                    console.log(matriz[i])
+                }
+                break;
+            case 3:
+                var busc = window.prompt("Digite o nome do registro a ser procurado")
+                for(i=0;i<matriz.length;i++){
+                    if(matriz[i][0]==busc){
+                        window.alert('Registro encontrado, cheque o console')
+                        console.log(matriz[i])
+                    }
+                }
+                break;
+            case 4:
+                var maior=[0,0,0];
+                for(a=0;a<matriz.length;a++){
+                    if(matriz[a][2]>maior[2]){
+                        maior=matriz[a];
+                    }
+                }
+                var busc = window.prompt("Digite o nome do registro a ser procurado")
+                for(i=0;i<matriz.length;i++){
+                    if(matriz[i][0]==busc){
+                        window.alert('Registro encontrado!');
+                        if(maior[0]==busc){
+                            window.alert("Você ganhou um cashback no valor de 10% da sua compra!!!")
+                        } else{
+                            window.alert("Você ganhou um cupom de 5% de desconto na sua próxima compra (máximo de desconto 25 reais)!!!")
+                        }
+                    }
+                }
+                break;
+            case 5:
+                window.alert('Sair...')
+                break;
+            default:
+                window.alert('Opção inválida');
+                break;
+        }
+        
+    } while(opc!=5);
+}
