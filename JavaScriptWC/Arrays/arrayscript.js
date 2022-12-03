e685c1b7d1ec0c16031be27f38ade0e9f0305f16
Guilherme Freitas <guilhermefsantos9@gmail.com>
2022-12-03 10:17:10 -0300

    Implementando 2° programa simples de console com prompts :(

--- JavaScriptWC/Arrays/arrayscript.js
+++ JavaScriptWC/Arrays/arrayscript.js
@@ -58,9 +58,11 @@ function buyStore() {
                 break;
             case 3:
                 var busc = window.prompt("Digite o nome do registro a ser excluído")
-                if(matriz[i][0]==busc){
-                    matriz[i]=null;
-                    window.alert('Registro encontrado e excluído!')
+                for(i=0;i<matriz.length;i++){
+                    if(matriz[i][0]==busc){
+                        matriz[i]=null;
+                        window.alert('Registro encontrado e excluído!')
+                    }
                 }
                 break;
             case 4:
@@ -74,6 +76,71 @@ function buyStore() {
                     if(matriz[i][0]==busc){
                         window.alert('Registro encontrado, cheque o console')
                         console.log(matriz[i])
+                    }
+                }
+                break;
+            case 6:
+                window.alert('Sair...')
+                break;
+            default:
+                window.alert('Opção inválida');
+                break;
+        }
+        
+    } while(opc!=6);
+}
+
+function Users() {
+    var matriz = [];
+    do {
+        var opc = Number(window.prompt(`
+            Escolha a opção desejada
+            [1]-Inserir novo registro
+            [2]-Editar Registro
+            [3]-Excluir Registro
+            [4]-Ver registros no console
+            [5]-Procurar registros por nome
+            [6]-Sair
+        `));
+        switch (opc) {
+            case 1:
+                var name = window.prompt("Digite o nome do cliente");
+                var listaaux = []
+                listaaux.push(name);
+                matriz.push(listaaux)
+                break;
+            case 2:
+                var busc = window.prompt("Digite o nome do registro a ser editado")
+                for(i=0;i<matriz.length;i++){
+                    if(matriz[i][0]==busc){
+                        window.alert('Registro encontrado!')
+                        var name = window.prompt("Digite o nome do cliente");
+                        var listaaux = []
+                        listaaux.push(name);
+                        matriz[i]=listaaux
+                    }
+                }
+                break;
+            case 3:
+                var busc = window.prompt("Digite o nome do registro a ser excluído")
+                for(i=0;i<matriz.length;i++){
+                    if(matriz[i][0]==busc){
+                        matriz[i]=null;
+                        window.alert('Registro encontrado e excluído!');
+                    }
+                }
+                break;
+            case 4:
+                for(i=0;i<matriz.length;i++){
+                    console.log(matriz[i]);
+                }
+                break;
+            case 5:
+                var busc = window.prompt("Digite o nome do registro a ser procurado");
+                for(i=0;i<matriz.length;i++){
+                    if(matriz[i][0]==busc){
+                        window.alert('Registro encontrado, cheque o console');
+                        console.log(matriz[i]);
                     }
                 }
                 break;
